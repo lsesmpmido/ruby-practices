3056f1144357ad30bb5bf502e9269c5306cc2b43
r.kawano <r.kawano@genbasupport.com>
2024-07-30 16:01:16 +0900

    fizzbuzz.rbを追加
    ・3の倍数のとき
    ・5の倍数のとき
    ・3の倍数でも5の倍数でもないとき
    の3パターンで処理をするようにコードを作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,11 @@
+(1..20).each do |num|
+  text = ''
+  # 3の倍数のとき
+  text += 'Fizz' if (num % 3).zero?
+  # 5の倍数のとき
+  text += 'Buzz' if (num % 5).zero?
+  # 3の倍数でも5の倍数でもないとき
+  text = num unless (num % 3).zero? || (num % 5).zero?
+
+  puts text
+end
