092260b09000b2177623149a1f731391a30263f1
r.kawano <r.kawano@genbasupport.com>
2024-08-05 09:56:02 +0900

    コードと同じことを言い換えているだけのコメントを削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,11 +2,8 @@
 
 1.upto(20) do |num|
   text = ''
-  # 3の倍数のとき
   text += 'Fizz' if (num % 3).zero?
-  # 5の倍数のとき
   text += 'Buzz' if (num % 5).zero?
-  # 3の倍数でも5の倍数でもないとき
   text = num unless (num % 3).zero? || (num % 5).zero?
 
   puts text
