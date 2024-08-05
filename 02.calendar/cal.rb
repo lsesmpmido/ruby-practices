218f2420859fac10fffeddb8478bae8e5aa4bcdf
r.kawano <r.kawano@genbasupport.com>
2024-08-05 10:48:41 +0900

    無くても伝わるコメントを削除（コードと同じことを言い換えているだけのコメントなど）

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,25 +2,21 @@
 require 'optparse'
 require 'date'
 
-# オプションのデフォルト値を設定
 today = Date.today
 options = {
   month: today.month,
   year: today.year
 }
 
-# オプションの値をコマンドラインから取得
 opt = OptionParser.new
 opt.on('-m', '--month MONTH') { |m| options[:month] = m.to_i }
 opt.on('-y', '--year YEAR') { |y| options[:year] = y.to_i }
 opt.parse!(ARGV)
 
-# 日付を取得
 first_date = Date.new(options[:year], options[:month], 1)
 last_date = Date.new(options[:year], options[:month], -1)
 days = [*1..last_date.day]
 
-# カレンダーを出力
 puts "      #{options[:month]}月 #{options[:year]}"
 puts '日 月 火 水 木 金 土'
 first_date.wday.times { printf '   ' }
