70624b1e24dcb2993c0c9773111b304c4d5d3828
r.kawano <r.kawano@genbasupport.com>
2024-08-28 09:17:36 +0900

    最大表示列数、リストの表示幅を定数として設定するように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+COLUMN_COUNT = 3
+LIST_WIDTH = 18
+
 def main
   files = Dir.glob('*')
-  column_count = 3
-  show_file_list(files, column_count)
+  show_file_list(files, COLUMN_COUNT)
 end
 
 def show_file_list(files, column_count)
   row_count = files.size.ceildiv(column_count)
   row_count.times do |row|
     column_count.times do |column|
-      print files[row + row_count *column].to_s.ljust(18)
+      print files[row + row_count *column].to_s.ljust(LIST_WIDTH)
     end
     puts
   end
