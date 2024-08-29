2dbd90ad32d0451a3b55c48e0513d7cd7f527f42
r.kawano <r.kawano@genbasupport.com>
2024-08-29 14:35:03 +0900

    -aオプションで隠しファイルが表示されるように処理を追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,12 +1,23 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 COLUMN_COUNT = 3
 LIST_WIDTH = 18
 
 def main
-  files = Dir.glob('*')
-  show_file_list(files, COLUMN_COUNT)
+  options = {}
+  opts = OptionParser.new
+  opts.on('-a', '--all', 'List all files') { options[:all] = true }
+  opts.parse(ARGV)
+
+  files = if options[:all]
+            Dir.entries('.')
+          else
+            Dir.glob('*')
+          end
+  show_file_list(files.sort, COLUMN_COUNT)
 end
 
 def show_file_list(files, column_count)
