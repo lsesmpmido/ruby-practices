b414bbe97f472781d48c327dfda2bd07f4a6d0e1
r.kawano <r.kawano@genbasupport.com>
2024-09-09 11:17:32 +0900

    total情報を管理するtotal_infoを追加するように修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -20,13 +20,20 @@ end
 
 options = {}
 file_info = { lines: [], words: [], bytes: [] }
+total_info = { lines: [], words: [], bytes: [] }
 paths = load_option(options).empty? ? [''] : load_option(options)
 paths.each { |path| add_info(File.read(path), file_info) }
 
+file_info.each do |key, array|
+  total_info[key] << array.sum
+end
 paths.each_with_index do |path, index|
   print "#{file_info[:lines][index].to_s.rjust(file_info[:lines].sum.to_s.size)} " if options[:lines] || options.empty?
   print "#{file_info[:words][index].to_s.rjust(file_info[:words].sum.to_s.size)} " if options[:words] || options.empty?
   print "#{file_info[:bytes][index].to_s.rjust(file_info[:bytes].sum.to_s.size)} " if options[:bytes] || options.empty?
   puts  path
 end
-puts "#{file_info[:lines].sum} #{file_info[:words].sum} #{file_info[:bytes].sum} total" unless paths.size.equal?(1)
+print "#{total_info[:lines][0].to_s.rjust(total_info[:lines].sum.to_s.size)} " if options[:lines] || options.empty?
+print "#{total_info[:words][0].to_s.rjust(total_info[:words].sum.to_s.size)} " if options[:words] || options.empty?
+print "#{total_info[:bytes][0].to_s.rjust(total_info[:bytes].sum.to_s.size)} " if options[:bytes] || options.empty?
+puts  "total"
