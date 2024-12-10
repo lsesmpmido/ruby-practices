758573ce36be4f086175a28c971bc4511045b4de
r.kawano <r.kawano@genbasupport.com>
2024-12-10 16:44:14 +0900

    Frameの初期化でShotオブジェクトのまま扱えるように修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -4,20 +4,11 @@ require_relative 'shot'
 
 class Frame
   def initialize(first_mark, second_mark, third_mark = nil)
-    @first_shot = Shot.new(first_mark).convert_score
-    @second_shot = Shot.new(second_mark).convert_score
-    @third_shot = Shot.new(third_mark).convert_score
+    @frame = [Shot.new(first_mark), Shot.new(second_mark), Shot.new(third_mark)].compact
   end
 
-  def calc_score(number_of_shots = 3)
-    case number_of_shots
-    when 1
-      @first_shot
-    when 2
-      @first_shot + @second_shot
-    when 3
-      @first_shot + @second_shot + @third_shot
-    end
+  def calc_score(number_of_shots = @frame.size)
+    @frame.take(number_of_shots).sum(&:convert_score)
   end
 
   def strike?
@@ -25,6 +16,6 @@ class Frame
   end
 
   def spare?
-    calc_score(2) == 10
+    calc_score(2) == 10 && !strike?
   end
 end
