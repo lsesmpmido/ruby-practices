5796550f34f2a2c8e387a8899e7c00e463a328b4
r.kawano <r.kawano@genbasupport.com>
2024-12-18 15:34:56 +0900

    実際のlsコマンドには存在しないオプション（`--long`）を削除するように修正

--- 07.ls_object/ls_object.rb
+++ 07.ls_object/ls_object.rb
@@ -8,7 +8,7 @@ options = { all: false, reverse: false, long: false }
 opts = OptionParser.new
 opts.on('-a', '--all', 'List all files') { options[:all] = true }
 opts.on('-r', '--reverse', 'List files in reverse order') { options[:reverse] = true }
-opts.on('-l', '--long', 'List long files') { options[:long] = true }
+opts.on('-l', 'List long files') { options[:long] = true }
 opts.parse(ARGV)
 
 ls = ListSegment.new(options)
